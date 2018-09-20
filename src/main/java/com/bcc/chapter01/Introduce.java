f8001a48217650d3f3d2c79d3637399aefca0cdf
ghbaqi <1562073264@qq.com>
2018-09-20 22:42:59 +0800

    chapter01 Java8 引入的背景

---
+++ src/main/java/com/bcc/chapter01/Introduce.java
@@ -0,0 +1,15 @@
+package com.bcc.chapter01;
+
+/**
+ * 1. 为什么要引入 Java8
+ *    多核和处理大型数据集 , 软件跟上硬件 . 更好的利用多核处理器
+ * 2. Java8 可以编写简洁又易于维护的代码
+ * 3. 函数是一等值 ; 函数式编程 , 传递代码
+ * 4. collection 主要为了存储和访问数据 , stream 主要用于描述对数据的计算 .
+ * 5. 接口中允许有默认方法
+ * 6. Optional 的引入
+ *
+ */
+public class Introduce {
+
+}
